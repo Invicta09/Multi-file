065497f2f8915eec4c1cba4174830134c2edf9c5
prabhjotb27 <prabhjotb009@gmail.com>
2022-12-05 00:55:24 +0530

    changes

---
+++ tables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>//standard header files
+#include"header1.h"//header filer specific to the program
+
+void tables(int a)
+{
+	printf("----------------------------------------------------------\n");
+	printf("Is printed using a second file\n");
+	for (int i=1;i<=tabletimes;i++)
+    {
+        printf("%d X %d = %d\n",a,i,a*i);
+    }
+    leap(primecheck);//defined in header file
+}
